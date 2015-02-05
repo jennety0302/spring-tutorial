8748f8e4ddce73c5f293c2f43efc04aba36f2287
AMahdy AbdElAziz <amahdy7@gmail.com>
2015-02-05 14:14:53 +0200

    Initial UI.
    Change-Id: I04cd399cd97e6532e1a6824cb627fbcb40e96865

---
+++ src/main/java/com/vaadin/spring/tutorial/MyVaadinUI.java
@@ -0,0 +1,26 @@
+package com.vaadin.spring.tutorial;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+
+@Theme("valo")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        setContent(new Label("This is MyVaadinUI"));
+    }
+
+}
